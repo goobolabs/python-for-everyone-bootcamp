6a45e563149629be387cab0e846b17d7be54bb13
Abdikheir <abdikheirmohammed43@gmail.com>
2026-04-21 23:14:57 +0200

    Assignment 1 Submission abdikheir

---
+++ submissions/abdikheirmohammed43-web/assignment1/assignment1.py
@@ -0,0 +1,35 @@
+# xisaabinta dhibacda
+name = input("qor magacaaga? ")
+print(f"kusoo dhowoow, {name}")
+score = 0
+answer = input("maxaad ka ka aaminsantahey python? ")
+if answer == "luqad fudud":
+    print(" waad saxantahey")
+    score += 1
+elif answer == "luqad adag":
+    print("ma adka python")
+    score += 1
+else:
+    print(" waad isku daydey mahadsanid")
+
+
+answer = input("python goormee la sameeyey?")
+if answer == "1991":
+    print("waad saxantahey")
+    score += 1
+elif answer == "1998":
+    print("lama sameynin 1998")
+else:
+    print("waad isku daydey mahadsanid")
+
+
+answer = input("mala baran karaa python? ")
+if answer == "haa":
+    print("waad saxantahey")
+    score += 1
+elif answer == "maya":
+    print("maya, waa la baran karaa")
+else:
+    print("waad isku daydey mahadsanid")
+
+print(f" mahadsanid {name}, dhibcahada waa {score} out of 3.")
