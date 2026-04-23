97ff8121468f4662bc94772023a90dc7acd52182
mrfiiqane <maxamedfx064@gmail.com>
2026-04-23 11:18:10 +0300

    homework01 and his assigments done

---
+++ submissions/mrfiiqane/homework 01/pseudocode_practice.py
@@ -0,0 +1,13 @@
+
+
+# Assignment 4: Pseudocode then code
+
+name = "Maxamed maxamuud"
+pet = "Cat"
+
+print("My name is", name)
+print("And my pet is a", pet)
+
+# output
+# My name is Maxamed maxamuud
+# And my pet is a Cat
---
+++ submissions/mrfiiqane/readme.md
@@ -0,0 +1,10 @@
+# **asc,  waxan salamaya ardeyda iyo macaliminta sharafdin iyo omar**
+
+ardeyda folderkan aniga leh /mrfiiqane yaan la iga taaban
+
+
+## **my submits:**
+
+homework 01:  **done**
+
+homework 02:
