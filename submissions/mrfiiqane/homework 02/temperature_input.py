5d8a95eae983371018995c294a6132a989211db2
mrfiiqane <maxamedfx064@gmail.com>
2026-04-23 19:13:48 +0300

    ass01

---
+++ submissions/mrfiiqane/homework 02/temperature_input.py
@@ -0,0 +1,13 @@
+
+# Assignment 1: Temperature from the user
+
+temperature = int(input("Enter temperature: "))
+
+if temperature >= 25:
+    print("warm")
+elif temperature >= 15:
+    print("ok")
+else:
+    print("cold")
+
+
